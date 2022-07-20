80b36d07947f245908819c5d25e7bf4bff9e229a
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-07-20 08:17:40 +0100

    Project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Search from './components/search/Search'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>FCC React Weather App</h2>
+      <Search />
+    </div>
+  )
+}
+
+export default App
