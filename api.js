c9cd252db9ac6a9c7d444bbe3535269b06187347
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-07-22 21:27:15 +0100

    Retrieving a city from GeoDB Cities API

--- api.js
+++ api.js
@@ -1,10 +1,11 @@
 export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': import.meta.env.VITE_X-RapidAPI-Key,
-		'X-RapidAPI-Host': import.meta.env.VITE_X-RapidAPI-Host,
-	}
-};
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': import.meta.env.VITE_X_RapidAPI_Key,
+    'X-RapidAPI-Host': import.meta.env.VITE_X_RapidAPI_Host,
+  },
+}
 
-export const GEO_API_URL= "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions"
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
  
+
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,30 +1,40 @@
 import React,{useState} from 'react'
 import { AsyncPaginate } from 'react-select-async-paginate'
+import { GEO_API_URL, geoApiOptions } from '../../../api'
 
-const Search = ({onSearchChange}) => {
-  const [search,setSearch] = useState(null)
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null)
 
-  const loadOptions = (inpuValue) =>{
-  return fetch('/cities',options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err=> console.log(err))
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            }
+          }),
+        }
+      })
+      .catch((err) => console.log(err))
   }
-  const handleOnChange = (searchData)=>{
+  const handleOnChange = (searchData) => {
     setSearch(searchData)
-    onSearchChange(searchData);
+    onSearchChange(searchData)
   }
-  
 
- 
   return (
-    <AsyncPaginate 
-    placeholder="Search for city"
-    debounceTimeout={600} 
-    value={search}
-    onChange={handleOnChange}
-    loadOptions={loadOptions}
-
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
     />
   )
 }
