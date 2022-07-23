85db156edd7984ee999638974bb09c9671367a47
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-07-23 15:06:01 +0100

    Setting up the two fetch queries

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import './App.css'
+import { useState } from 'react'
 import Search from './components/search/Search'
 import CurrentWeather from './components/current-weather/current-weather'
+import { WEATHER_API_URL } from '../api'
+import './App.css'
 
 function App() {
-  const handleOnSearchChange = (searchData) =>{
-    console.log(searchData)
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [forecast, setForecast] = useState(null)
 
-      }
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(' ')
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${
+        import.meta.env.VITE_OPEN_WEATHER_KEY
+      }`
+    )
+
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${
+        import.meta.env.VITE_OPEN_WEATHER_KEY
+      }`
+    )
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json()
+        const forecastResponse = await response[1].json()
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse })
+        setForecast({ city: searchData.label, ...forecastResponse })
+      })
+      .catch(console.log)
+  }
+
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      <CurrentWeather />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   )
 }
